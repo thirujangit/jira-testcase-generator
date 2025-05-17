fdffc55c999112d28f986ac961fd5903a85087d7
Thirupathi Jangapally <thirupathi.jangapally@growtharc.com>
2025-05-17 06:22:04 +0530

    Fix: moved /panel route before app.run()

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def panel():
     return render_template("panel.html", issue_key=issue_key)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
